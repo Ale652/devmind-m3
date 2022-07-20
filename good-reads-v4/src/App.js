fe2a3cd936cb1f3fee603315f35322eb576029a8
al moise <mmoisealexandra42@gmail.com>
2022-07-20 13:12:18 +0300

    + get token

---
+++ good-reads-v4/src/App.js
@@ -0,0 +1,101 @@
+
+import "./App.css";
+import AddBook from "./components/add-book/AddBook";
+import BooksList from "./components/books-list/BooksList";
+import Menu from "./components/menu/Menu";
+import {Routes, Route} from "react-router-dom";
+import Profile from "./components/profile/Profile";
+
+import { BrowserRouter } from 'react-router-dom';
+import Register from "./components/register/Register";
+import Login from "./components/login/Login";
+
+function App() {
+  return (
+      <div className="App">
+        {/* <Menu />
+        <AddBook />
+        <BooksList /> */}
+        <BrowserRouter>
+            <Menu/>
+                <Routes>
+                    <Route path="/addBook" element={<AddBook/>}/>
+                </Routes>
+                <Routes>
+                    <Route path="/Dashboard" element={<BooksList/>}/>
+                </Routes>
+                <Routes>
+                    <Route path="/profile" element={<Profile/>}/>
+                </Routes>
+                <Routes>
+                    <Route path="/register" element={<Register/>}/>
+                </Routes>
+                <Routes>
+                    <Route path="/login" element={<Login/>}/>
+                </Routes>
+        </BrowserRouter>
+      </div>
+  );
+}
+
+export default App;
+
+
+const notes = [
+    {
+        title: "sal",
+        description:
+            "dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss",
+    },
+    {
+        title: "sal",
+        description:
+            "dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss",
+    },
+    {
+        title: "sal",
+        description:
+            "dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss",
+    },
+    {
+        title: "sal",
+        description:
+            "dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss",
+    },
+
+    {
+        title: "sal",
+        description:
+            "dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss",
+    },
+    {
+        title: "sal",
+        description:
+            "dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss",
+    },
+    {
+        title: "sal",
+        description:
+            "dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss",
+    },
+    {
+        title: "sal",
+        description:
+            "dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss",
+    },
+    {
+        title: "sal",
+        description:
+            "dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss",
+    },
+    {
+        title: "sal",
+        description:
+            "dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss",
+    },
+    {
+        title: "sal",
+        description:
+            "dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss dadasdss, f5b042 f5b042, dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdss dadasdssdadasdss",
+    },
+];
---
+++ good-reads-v4/src/components/redux/reducers/reducers.js
@@ -0,0 +1,70 @@
+import { ADD_BOOK, GET_BOOKS, REGISTER, LOGIN } from "../actions/actions";
+
+const initialState = {
+  books: [],
+  register: {},
+  login: {}
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    case REGISTER:
+      return {
+        
+            ...state,
+            register:
+                        {
+                            email: action.payload.email,
+                            role: action.payload.role,
+                            password: action.payload.password,
+                        }
+                    ,
+        }; break;
+
+        case LOGIN:
+            return {
+                
+                    ...state,
+                    login:
+                                {
+                                    email: action.payload.email,
+                                    role: action.payload.role,
+                                    token: action.payload.token,
+                                }
+                            ,
+                }; break;
+
+
+
+
+    case GET_BOOKS:
+      return {
+        ...state,
+      books: action.payload.data,
+    }; break;
+
+
+
+    case ADD_BOOK:
+      return {
+        ...state,
+        books: [
+          ...state.books,
+          {
+            title: action.payload.title,
+            description: action.payload.description,
+            type: action.payload.type,
+            publishedDate: action.payload.publishedDate,
+            author_id: action.payload.author_id,
+            id: action.payload.id,
+          },
+        ],
+      }; break;
+
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
